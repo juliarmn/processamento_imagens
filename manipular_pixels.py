757900d3fd56ab2c799c759727a8e9410dddd5b4
juliarmn <julia.rm.nascimento@unesp.br>
2023-03-19 13:55:58 -0300

    Arruma programa

--- manipular_pixels.py
+++ manipular_pixels.py
@@ -2,8 +2,8 @@ import cv2
 
 imagem = cv2.imread("imagem.jpg")#BGR - open cv
 print(imagem)
-# cv2.imshow("Imagem", imagem)
-# cv2.waitKey(0)
+cv2.imshow("Imagem", imagem)
+cv2.waitKey(0)
 
 #Alterando valores de pixels:
 imagem[0][0] = (255, 255, 255) #tupla
@@ -12,8 +12,8 @@ imagem[0][0] = (255, 255, 255) #tupla
 
 #Mudar mais de um pixel
 imagem[0:10, 0:10] = (255, 255, 255)
-# cv2.imshow("Imagem", imagem)
-# cv2.waitKey(0)
+cv2.imshow("Imagem", imagem)
+cv2.waitKey(0)
 
 #Alterar dinamicamente os pixels com estruturas de repetição:
 branco = (255, 255, 255)
@@ -21,9 +21,9 @@ preto = (0, 0, 0)
 for i in range(0, imagem.shape[0]):
   for j in range(0, imagem.shape[1]):
     imagem[i][j] = branco
-#Todos os pixels brancos:
-# cv2.imshow("Imagem", imagem)
-# cv2.waitKey(0)
+Todos os pixels brancos:
+cv2.imshow("Imagem", imagem)
+cv2.waitKey(0)
 
 #Pulando de 10 em 10 pixels:
 #No caso, como modificamos a imagem anteriormente, ficam pontos pretos na imagem branca
@@ -32,22 +32,22 @@ for i in range(0, imagem.shape[0], 10):
   for j in range(0, imagem.shape[1], 10):
     imagem[i][j] = preto
 #Todos os pixels brancos:
-# cv2.imshow("Imagem", imagem)
-# cv2.waitKey(0)
+cv2.imshow("Imagem", imagem)
+cv2.waitKey(0)
 
 #Só mexendo na altura:
 for i in range(0, imagem.shape[0], 10):
   for j in range(0, imagem.shape[1]):
     imagem[i][j] = preto
-# cv2.imshow("Imagem", imagem)
-# cv2.waitKey(0)
+cv2.imshow("Imagem", imagem)
+cv2.waitKey(0)
 
 #Só mexendo na largura:
 for i in range(0, imagem.shape[0]):
   for j in range(0, imagem.shape[1], 10):
     imagem[i][j] = preto
-# cv2.imshow("Imagem", imagem)
-# cv2.waitKey(0)
+cv2.imshow("Imagem", imagem)
+cv2.waitKey(0)
 
 #Observe que formaram quadradinhos
 
@@ -79,8 +79,8 @@ cv2.waitKey(0)
 imagem = cv2.imread("imagem.jpg")#BGR - open cv
 fonte = cv2.FONT_HERSHEY_PLAIN#Fonte da palavra
 cv2.putText(imagem, "Olá", (50, 50), fonte, 2, (255,255,255), 2)#imagem, texto, pixel onde inicia, fonte, expessura, cor, tamanho
-# cv2.imshow("Imagem", imagem)
-# cv2.waitKey(0)
+cv2.imshow("Imagem", imagem)
+cv2.waitKey(0)
 
 #Desenho da escrita bem pixelado, para arrumar:
 cv2.putText(imagem, "Jua", (50, 50), fonte, 2, (255,255,255), 2, cv2.LINE_AA)
