4b21bab0dd9ead627f8177ba6c5288c51fd2bf8d
juliarmn <julia.rm.nascimento@unesp.br>
2023-03-19 13:55:33 -0300

    Arruma programa

--- manipular_pixels.py
+++ manipular_pixels.py
@@ -2,8 +2,8 @@ import cv2
 
 imagem = cv2.imread("imagem.jpg")#BGR - open cv
 print(imagem)
-cv2.imshow("Imagem", imagem)
-cv2.waitKey(0)
+# cv2.imshow("Imagem", imagem)
+# cv2.waitKey(0)
 
 #Alterando valores de pixels:
 imagem[0][0] = (255, 255, 255) #tupla
@@ -12,8 +12,8 @@ imagem[0][0] = (255, 255, 255) #tupla
 
 #Mudar mais de um pixel
 imagem[0:10, 0:10] = (255, 255, 255)
-cv2.imshow("Imagem", imagem)
-cv2.waitKey(0)
+# cv2.imshow("Imagem", imagem)
+# cv2.waitKey(0)
 
 #Alterar dinamicamente os pixels com estruturas de repetição:
 branco = (255, 255, 255)
@@ -22,8 +22,8 @@ for i in range(0, imagem.shape[0]):
   for j in range(0, imagem.shape[1]):
     imagem[i][j] = branco
 #Todos os pixels brancos:
-cv2.imshow("Imagem", imagem)
-cv2.waitKey(0)
+# cv2.imshow("Imagem", imagem)
+# cv2.waitKey(0)
 
 #Pulando de 10 em 10 pixels:
 #No caso, como modificamos a imagem anteriormente, ficam pontos pretos na imagem branca
@@ -32,22 +32,22 @@ for i in range(0, imagem.shape[0], 10):
   for j in range(0, imagem.shape[1], 10):
     imagem[i][j] = preto
 #Todos os pixels brancos:
-cv2.imshow("Imagem", imagem)
-cv2.waitKey(0)
+# cv2.imshow("Imagem", imagem)
+# cv2.waitKey(0)
 
 #Só mexendo na altura:
 for i in range(0, imagem.shape[0], 10):
   for j in range(0, imagem.shape[1]):
     imagem[i][j] = preto
-cv2.imshow("Imagem", imagem)
-cv2.waitKey(0)
+# cv2.imshow("Imagem", imagem)
+# cv2.waitKey(0)
 
 #Só mexendo na largura:
 for i in range(0, imagem.shape[0]):
   for j in range(0, imagem.shape[1], 10):
     imagem[i][j] = preto
-cv2.imshow("Imagem", imagem)
-cv2.waitKey(0)
+# cv2.imshow("Imagem", imagem)
+# cv2.waitKey(0)
 
 #Observe que formaram quadradinhos
 
@@ -59,8 +59,8 @@ for i in range(0, imagem.shape[0], 10):
   for j in range(0, imagem.shape[1], 10):
     #Aumenta o tamanho dos quadradinhos formados:
     imagem[i:i+5, j:j+5] = branco
-cv2.imshow("Imagem", imagem)
-cv2.waitKey(0)
+# cv2.imshow("Imagem", imagem)
+# cv2.waitKey(0)
 
 #A partir de condições:
 imagem = cv2.imread("imagem.jpg")#BGR - open cv
@@ -69,7 +69,7 @@ for i in range(0, imagem.shape[0], 10):
   for j in range(0, imagem.shape[1], 10):
     #Lembre que open cv é BGR
     #Aonde for azul puro fica branco->pontos brancos na imagem
-    if(imagem[i][j][0] == 255):
+    if imagem[i][j][0] <= 255 and imagem[i][j][0] > 30:
       imagem[i][j] = branco
 cv2.imshow("Imagem", imagem)
 cv2.waitKey(0)
@@ -78,9 +78,9 @@ cv2.waitKey(0)
 #Usa um método do open cv
 imagem = cv2.imread("imagem.jpg")#BGR - open cv
 fonte = cv2.FONT_HERSHEY_PLAIN#Fonte da palavra
-cv2.putText(imagem, "Jua", (50, 50), fonte, 2, (255,255,255), 2)#imagem, texto, pixel onde inicia, fonte, expessura, cor, tamanho
-cv2.imshow("Imagem", imagem)
-cv2.waitKey(0)
+cv2.putText(imagem, "Olá", (50, 50), fonte, 2, (255,255,255), 2)#imagem, texto, pixel onde inicia, fonte, expessura, cor, tamanho
+# cv2.imshow("Imagem", imagem)
+# cv2.waitKey(0)
 
 #Desenho da escrita bem pixelado, para arrumar:
 cv2.putText(imagem, "Jua", (50, 50), fonte, 2, (255,255,255), 2, cv2.LINE_AA)
